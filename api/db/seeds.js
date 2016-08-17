70a5f8e694992f16e463e7362ae85a30091d99f4
Paul Crosby <paul71crosby@gmail.com>
2016-08-17 10:47:39 +0100

    added data and amended schools dropdown

--- api/db/seeds.js
+++ api/db/seeds.js
@@ -5,6 +5,8 @@ mongoose.connect(databaseURL);
 
 var Member = require("../models/member"); 
 
+// TO DROP DATABASE COLLECTION MEMBER
+// Member.collection.drop();
 
 var Member1 = new Member({
   name: "Harry Potter",
@@ -31,7 +33,7 @@ Member2.save(function(err, member) {
 var Member3 = new Member({
   name: "Arnie  Schwartz",
   email: "as@gmail.com",
-  school: "Kindergarten kop school"
+  school: "Kindergarten Kop School"
 })
 
 Member3.save(function(err, member) {
@@ -50,3 +52,68 @@ Member4.save(function(err, member) {
   console.log(member);
 })
 
+var Member5 = new Member({
+  name: "Gus Stewart",
+  email: "gus@eton.com",
+  school: "Eton"
+});
+
+Member5.save(function(err, member) {
+  if (err) return console.log(err);
+  console.log(member);
+})
+
+var Member6 = new Member({
+  name: "Chuck Norris",
+  email: "chucky@h.com",
+  school: "Harvard"
+});
+
+Member6.save(function(err, member) {
+  if (err) return console.log(err);
+  console.log(member);
+})
+
+var Member7 = new Member({
+  name: "Sylvester Stallone",
+  email: "ss@h.com",
+  school: "Harvard"
+});
+
+Member7.save(function(err, member) {
+  if (err) return console.log(err);
+  console.log(member);
+})
+
+var Member8 = new Member({
+  name: "Prince William",
+  email: "hrh@eton.com",
+  school: "Eton"
+});
+
+Member8.save(function(err, member) {
+  if (err) return console.log(err);
+  console.log(member);
+})
+
+var Member9 = new Member({
+  name: "Jason Kenny",
+  email: "jasonh@gold.com",
+  school: "Gold School"
+});
+
+Member9.save(function(err, member) {
+  if (err) return console.log(err);
+  console.log(member);
+})
+
+var Member10 = new Member({
+  name: "Laura Trott",
+  email: "laurah@gold.com",
+  school: "Gold School"
+});
+
+Member10.save(function(err, member) {
+  if (err) return console.log(err);
+  console.log(member);
+})
